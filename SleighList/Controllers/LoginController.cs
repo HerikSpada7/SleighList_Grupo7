a65165f31aaf00d4d85d541afd4298e22c6abf4b
Herik Spada <gryzhn1@gmail.com>
2024-12-12 11:50:46 -0300

    Projeto MVC SleighList

---
+++ SleighList/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SleighList.Controllers
+{
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
